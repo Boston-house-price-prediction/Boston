5aa205a3738bf31941496db5c275aaa1d2367a5e
Daren-Fan <s113029017@smail.nchu.edu.tw>
2024-11-14 14:56:39 +0800

    remove unused import for train_test_split in bank.py

--- bank.py
+++ bank.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
